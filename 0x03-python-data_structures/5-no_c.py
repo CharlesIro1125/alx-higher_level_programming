e03cfa18d0c76492cab721539da03b2765924cfe
Charles Obinna Iro <irocharles22@gmail.com>
2022-08-30 13:32:43 -0700

    update

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,5 +3,5 @@
 def no_c(my_string):
     if len(my_string) < 1:
         return (None)
-    copy_str = [x for x in my_string if x != 'c' or x != 'C']
+    copy_str = [x for x in my_string if x != 'c' or x != 'C' else pass]
     return ("".join(copy_str))
