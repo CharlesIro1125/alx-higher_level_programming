39e7788ea4e5ed04eeb10881af54f4619ce04d29
Charles Obinna Iro <irocharles22@gmail.com>
2022-10-03 08:24:08 -0700

    update

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-
 """ Defines a Rectangle."""
+
 
 class Rectangle:
 
     """represent a rectangle"""
-    def __init__(self, width = 0, height = 0):
+    def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
@@ -35,10 +35,12 @@ class Rectangle:
 
     def area(self):
         return self.__width * self.__height
+
     def perimeter(self):
         if (self.__width) == 0 or (self.__height) == 0:
             return 0
         return ((self.__width)*2) + ((self.__height)*2)
+
     def __str__(self):
         if (self.__width == 0) or (self.__height == 0):
            return ""
