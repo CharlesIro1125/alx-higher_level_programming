3040a59eacfdba9fb61a6033d0d085a88fc2c963
Charles Obinna Iro <irocharles22@gmail.com>
2022-10-03 07:21:34 -0700

    update

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,22 +1,16 @@
-#!/usr/bin/paython3
-"""Defining a Rectangle class."""
+#!/usr/bin/python3
+""" Defines a Rectangle."""
 
 
 class Rectangle:
-    """represent a rectangle"""
 
+    """represent a rectangle"""
     def __init__(self, width=0, height=0):
-        """Initialize a new Rectangle.
-        Args:
-            width (int): The width of the new rectangle.
-            height (int): The height of the new rectangle.
-        """
         self.__width = width
         self.__height = height
 
     @property
     def width(self):
-        """Get/set the width of the rectangle."""
         return self.__width
 
     @width.setter
@@ -29,7 +23,6 @@ class Rectangle:
 
     @property
     def height(self):
-        """Get/set the height of the rectangle."""
         return self.__height
 
     @height.setter
