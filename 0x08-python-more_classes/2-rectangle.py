3dcfa7afb4c5442e7741b495b2fc4426c142cdf9
Charles Obinna Iro <irocharles22@gmail.com>
2022-10-03 07:18:00 -0700

    update

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -5,7 +5,7 @@
 class Rectangle:
 
     """represent a rectangle"""
-    def __init__(self, width = 0, height = 0):
+    def __init__(self, width=0, height=0):
         self.__width = width
         self.__height = height
 
@@ -32,8 +32,9 @@ class Rectangle:
         elif value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-    
+
     def area(self):
         return self.__width * self.__height
+
     def perimeter(self):
         return ((self.__width)*2) + ((self.__height)*2)
