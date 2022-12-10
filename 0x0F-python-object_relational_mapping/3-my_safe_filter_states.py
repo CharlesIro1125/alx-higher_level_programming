d5abf0399cfad01b23e5c5916b85fc81b26d7d36
Charles Obinna Iro <irocharles22@gmail.com>
2022-12-09 19:10:02 -0800

    object relational mapping

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -6,12 +6,12 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    " select all states "
+    """ select all states """
 
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE %s
+    cur.execute("SELECT * FROM states WHERE name LIKE %s \
                 ORDER BY id ASC", (argv[4],))
     query_rows = cur.fetchall()
     for row in query_rows:
